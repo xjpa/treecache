be5357f196949f102b8ad67000c3eae3b5e5658d
xjpa <xjpa@proton.me>
2023-08-29 19:46:07 +0800

    add async support

--- run.js
+++ run.js
@@ -1,12 +1,18 @@
-var Database = require('./database').Database;
-var database = new Database();
+const { Database } = require('./database'); // Assuming the exported class is named Database
+const database = new Database();
 
-var id = database.insert({
-  userid: 1,
-  name: 'john patrick amata',
-  sex: 'male',
-});
-console.log(id);
+(async () => {
+  try {
+    const id = await database.insert({
+      userid: 1,
+      name: 'john patrick amata',
+      sex: 'male',
+    });
+    console.log(id);
 
-var record = database.findById(id);
-console.log(record);
+    const record = await database.findById(id);
+    console.log(record);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+})();
